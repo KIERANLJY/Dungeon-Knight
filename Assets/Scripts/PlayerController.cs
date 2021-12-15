1b4a41696285e56ebc355cb035875a5ff015229e
Your Name <you@example.com>
2021-12-15 17:23:58 +0000

    play jumping implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float runSpeed;
-    private Rigidbody2D myRigidBody;
-    private Animator myAnimator;
+    private float _runSpeed;
+    private float _jumpSpeed;
+    private bool _isGround;
+    private Rigidbody2D _rigidBody;
+    private Animator _animator;
+    private BoxCollider2D _feetCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        runSpeed = 5;
-        myRigidBody = GetComponent<Rigidbody2D>();
-        myAnimator = GetComponent<Animator>();
+        _runSpeed = 5;
+        _jumpSpeed = 6;
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _feetCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -20,21 +26,24 @@ public class PlayerController : MonoBehaviour
     {
         Flip();
         Run();
+        Jump();
+        CheckGround();
+        SwitchStatesInJumping();
     }
 
     // Change orientation of the play according to moving direction
     void Flip()
     {
-        bool playerHasXAxisSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        bool playerHasXAxisSpeed = Mathf.Abs(_rigidBody.velocity.x) > Mathf.Epsilon;
         if(playerHasXAxisSpeed)
         {
             // Move forward
-            if(myRigidBody.velocity.x > Mathf.Epsilon)
+            if(_rigidBody.velocity.x > Mathf.Epsilon)
             {
                 transform.localRotation = Quaternion.Euler(0,0,0);
             }
             // Move Backward
-            if(myRigidBody.velocity.x < -Mathf.Epsilon)
+            if(_rigidBody.velocity.x < -Mathf.Epsilon)
             {
                 transform.localRotation = Quaternion.Euler(0,180,0);
             }
@@ -45,13 +54,60 @@ public class PlayerController : MonoBehaviour
     void Run()
     {
         // Get the direction of player moving
-        float moveDir = Input.GetAxis("Horizontal");
+        float _moveDir = Input.GetAxis("Horizontal");
         // Set velocity and assign to the player
-        Vector2 playVel = new Vector2(moveDir * runSpeed, myRigidBody.velocity.y);
-        myRigidBody.velocity = playVel;
+        if (_isGround)
+        {
+            Vector2 _playVel = new Vector2(_moveDir * _runSpeed, _rigidBody.velocity.y);
+            _rigidBody.velocity = _playVel;
+        }
+        else
+        {
+            Vector2 _playVel = new Vector2(_moveDir * (_runSpeed - 2), _rigidBody.velocity.y);
+            _rigidBody.velocity = _playVel;
+        }
 
         // Check if the player is running
-        bool playerHasXAxisSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
-        myAnimator.SetBool("Run", playerHasXAxisSpeed);
+        bool _playerHasXAxisSpeed = Mathf.Abs(_rigidBody.velocity.x) > Mathf.Epsilon;
+        _animator.SetBool("Run", _playerHasXAxisSpeed);
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            // Jump if the player is on the ground
+            if (_isGround)
+            {
+                _animator.SetBool("Jump", true);
+                // Get velocity of jumping on Y axis
+                Vector2 _jumpVel = new Vector2(0, _jumpSpeed);
+                _rigidBody.velocity = _jumpVel;
+            }
+        }
+    }
+
+    void CheckGround()
+    {
+        _isGround = _feetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+    }
+
+    void SwitchStatesInJumping()
+    {
+        _animator.SetBool("Idle", false);
+        if (_animator.GetBool("Jump"))
+        {
+            // Reach the top
+            if (_rigidBody.velocity.y < 0)
+            {
+                _animator.SetBool("Jump", false);
+                _animator.SetBool("Fall", true);
+            }
+        }
+        else if (_isGround)
+        {
+            _animator.SetBool("Fall", false);
+            _animator.SetBool("Idle", true);
+        }
     }
 }
