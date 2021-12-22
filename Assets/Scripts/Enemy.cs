ab177c7d6f5a2612b486c949b0ac7943d703ff78
Your Name <you@example.com>
2021-12-22 00:32:21 +0000

    bat prefab created

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,16 @@ public abstract class Enemy : MonoBehaviour
 {
     public int _health;
     public int _damage;
+    private float _flashTime;
+    private SpriteRenderer _spriteRenderer;
+    private Color _originalColor;
 
     // Start is called before the first frame update
     public void Start()
     {
-        
+        _flashTime = 0.2f;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _originalColor = _spriteRenderer.color; 
     }
 
     // Update is called once per frame
@@ -30,5 +35,17 @@ public abstract class Enemy : MonoBehaviour
     public void TakeDamage(int damage)
     {
         _health -= damage;
+        FlashColor();
+    }
+
+    void FlashColor()
+    {
+        _spriteRenderer.color = Color.red;
+        Invoke("ResetColor", _flashTime);
+    }
+
+    void ResetColor()
+    {
+        _spriteRenderer.color = _originalColor;
     }
 }
--- Assets/Scripts/EnemyBat.cs
+++ Assets/Scripts/EnemyBat.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class EnemyBat : Enemy
 {
+    private float _speed;
+    private float _waitTime;
+    private float _timeCount;
+    public Transform _destination;
+    public Transform _areaBottomLeft;
+    public Transform _areaTopRight;
+
     // Start is called before the first frame update
     new void Start()
     {
+        base.Start();
         _health = 5;
         _damage = 1;
+        _speed = 1f;
+        _waitTime = 1f;
+        _timeCount = _waitTime;
+        _destination.position = GetRandomPos();
     }
 
     // Update is called once per frame
     new void Update()
     {
         base.Update();
+        transform.position = Vector2.MoveTowards(transform.position, _destination.position, _speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, _destination.position) <= 0.1f)
+        {
+            if (_timeCount <= 0)
+            {
+                _destination.position = GetRandomPos();
+                _timeCount = _waitTime;
+            }
+            else
+            {
+                _timeCount -= Time.deltaTime;
+            }
+        }
+    }
+
+    Vector2 GetRandomPos()
+    {
+        Vector2 _pos = new Vector2(Random.Range(_areaBottomLeft.position.x, _areaTopRight.position.x), Random.Range(_areaBottomLeft.position.y, _areaTopRight.position.y));
+        return _pos;
     }
 }
